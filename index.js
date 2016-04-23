1ab99666e07a4415bcdb650e6c159bfacb6ffdd3
Bob <negenentwintig@hotmail.com>
2016-04-23 17:36:46 +0200

    First real commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var sweet = require('sweet.js');
+var loaderUtils = require('loader-utils');
+
+module.exports = function(source) {
+	var config = loaderUtils.getLoaderConfig(this, "sweet");
+	
+	config.filename = loaderUtils.interpolateName(this, "[name].[ext]", {})
+	
+	this.cacheable()
+	
+	try {
+		var compiled = sweet.compile(source, config);
+		this.callback(null, compiled.code, compiled.sourceMap) // beware that sourceMaps are not yet supported
+	} catch (e) {
+		this.callback(e)
+	}
+};
